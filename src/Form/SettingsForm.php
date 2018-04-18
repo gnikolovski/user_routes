4d140caeed3aa714ea087d3ef4b891819904f6a1
Goran Nikolovski <goran.nikolovski@possible.com>
2018-04-18 09:24:52 +0200

    Initial commit

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Drupal\user_routes\Form;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteBuilderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines a form that configures User Routes settings.
+ */
+class SettingsForm extends ConfigFormBase implements ContainerInjectionInterface {
+
+  /**
+   * @var \Drupal\Core\Routing\RouteBuilderInterface
+   */
+  protected $routerBuilder;
+
+  /**
+   * Constructs a new SettingsForm object.
+   *
+   * @param \Drupal\Core\Routing\RouteBuilderInterface $router_builder
+   *   Route builder.
+   */
+  public function __construct(RouteBuilderInterface $router_builder) {
+    $this->routerBuilder = $router_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('router.builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'user_routes_admin_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'user_routes.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $user_routes_config = $this->config('user_routes.settings');
+
+    $form['user_login'] = [
+      '#type' => 'textfield',
+      '#title' => t('User login'),
+      '#default_value' => $user_routes_config->get('user_login'),
+      '#description' => t('Default route path: /user/login'),
+    ];
+
+    $form['user_register'] = [
+      '#type' => 'textfield',
+      '#title' => t('User register'),
+      '#default_value' => $user_routes_config->get('user_register'),
+      '#description' => t('Default route path: /user/register'),
+    ];
+
+    $form['user_logout'] = [
+      '#type' => 'textfield',
+      '#title' => t('User logout'),
+      '#default_value' => $user_routes_config->get('user_logout'),
+      '#description' => t('Default route path: /user/logout'),
+    ];
+
+    $form['user_pass'] = [
+      '#type' => 'textfield',
+      '#title' => t('User reset password'),
+      '#default_value' => $user_routes_config->get('user_pass'),
+      '#description' => t('Default route path: /user/password'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $this->config('user_routes.settings')
+         ->set('user_login', $values['user_login'])
+         ->set('user_register', $values['user_register'])
+         ->set('user_logout', $values['user_logout'])
+         ->set('user_pass', $values['user_pass'])
+         ->save();
+
+    $this->routerBuilder->rebuild();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
---
+++ src/Routing/RouteSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\user_routes\Routing;
+
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Alter user route paths.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a new RouteSubscriber object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactory $config_factory
+   *   The config factory.
+   */
+  public function __construct(ConfigFactory $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  protected function alterRoutes(RouteCollection $collection) {
+    $user_routes_config = $this->configFactory
+      ->get('user_routes.settings')
+      ->get();
+
+    foreach ($user_routes_config as $route_name => $route_path) {
+      if (!$route_path) {
+        continue;
+      }
+
+      $route_name = str_replace('_', '.', $route_name);
+      if ($route = $collection->get($route_name)) {
+        $route->setPath($route_path);
+      }
+    }
+  }
+
+}
